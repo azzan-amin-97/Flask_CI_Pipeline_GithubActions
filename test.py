73eac5d5418692bf97af2913a65f8a8656b9ef7a
Azzan Amin <ibnuamin97@gmail.com>
2020-10-08 14:04:23 +0800

    Update test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+import unittest
+
+import requests
+
+
+class BasicTests(unittest.TestCase):
+    def test_view_report(self):
+        # Given
+        num1 = 100
+        num2 = 200
+
+        # When
+        response = requests.post("http://127.0.0.1:5000/api/addition",
+                                 data={'num1': num1,
+                                       'num2': num2})
+
+        res_json = response.json()
+        result = res_json['result']
+
+        # Then
+        self.assertEqual(result, 300)
+
+if __name__ == "__main__":
+    unittest.main()
